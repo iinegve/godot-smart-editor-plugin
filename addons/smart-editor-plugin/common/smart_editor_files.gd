2a264ea6a63cd06014a975f83c4c1657a12e5397
Evgenii Morozov <evgeny.a.morozov@gmail.com>
2026-08-13 02:10:29 +0300

    Fixed path resolution for Windows

--- addons/smart-editor-plugin/common/smart_editor_files.gd
+++ addons/smart-editor-plugin/common/smart_editor_files.gd
@@ -3,8 +3,30 @@ extends RefCounted
 
 
 static func path_to_file_uri(path: String) -> String:
-	return "file://" + path.uri_encode().replace("%2F", "/")
+	var encoded_path := path.uri_encode().replace("%2F", "/").lstrip("/")
+	return "file:///" + encoded_path
 
 
 static func file_uri_to_path(uri: String) -> String:
-	return uri.trim_prefix("file://").uri_decode()
+	return _file_uri_to_path(uri, OS.has_feature("windows"))
+
+
+static func _file_uri_to_path(uri: String, is_windows: bool) -> String:
+	var path := uri.trim_prefix("file://").uri_decode()
+	if is_windows and _has_windows_drive_with_leading_slash(path):
+		return path.substr(1)
+
+	return path
+
+
+static func _has_windows_drive_with_leading_slash(path: String) -> bool:
+	if path.length() < 3 or path[0] != "/" or path[2] != ":":
+		return false
+	if path.length() > 3 and path[3] != "/":
+		return false
+
+	var drive_letter := path.unicode_at(1)
+	return (
+		(drive_letter >= "A".unicode_at(0) and drive_letter <= "Z".unicode_at(0))
+		or (drive_letter >= "a".unicode_at(0) and drive_letter <= "z".unicode_at(0))
+	)
--- test/unit/editor/smart_editor_files_test.gd
+++ test/unit/editor/smart_editor_files_test.gd
@@ -3,9 +3,24 @@ extends GdUnitTestSuite
 const SmartEditorFiles := preload("res://addons/smart-editor-plugin/common/smart_editor_files.gd")
 
 
-func test_file_uri_round_trip_keeps_spaces_and_slashes() -> void:
+func test_posix_file_uri_round_trip_keeps_spaces_and_slashes() -> void:
 	var path := "/Users/example/My Project/player.gd"
 	var uri := SmartEditorFiles.path_to_file_uri(path)
 
 	assert_str(uri).is_equal("file:///Users/example/My%20Project/player.gd")
 	assert_str(SmartEditorFiles.file_uri_to_path(uri)).is_equal(path)
+
+
+func test_windows_file_uri_places_drive_letter_in_local_path() -> void:
+	var path := "D:/Projects/My Project/src/player_spawner.gd"
+
+	assert_str(SmartEditorFiles.path_to_file_uri(path)).is_equal(
+		"file:///D%3A/Projects/My%20Project/src/player_spawner.gd"
+	)
+
+
+func test_windows_file_uri_round_trip_removes_uri_drive_slash() -> void:
+	var path := "D:/Projects/My Project/src/player_spawner.gd"
+	var uri := SmartEditorFiles.path_to_file_uri(path)
+
+	assert_str(SmartEditorFiles._file_uri_to_path(uri, true)).is_equal(path)
